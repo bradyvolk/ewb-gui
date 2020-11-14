6f339ca3ea229a174103a25099e3870499410539
bradyvolk <39806319+bradyvolk@users.noreply.github.com>
2020-11-14 15:19:27 -0500

    Adds line segment drawing

--- application/windows/MapWindow.py
+++ application/windows/MapWindow.py
@@ -30,6 +30,7 @@ from kivy.uix.popup import Popup
 from kivy.factory import Factory
 from kivy.uix.scatter import Scatter
 from kivy.graphics import Color
+from kivy.graphics import Line
 import widgets.MapWindowWidget
 
 
@@ -58,123 +59,53 @@ class MapWindow(Screen):
         self.dismiss_popup()
 
 
-class MapLayer(Widget):
-    """A map layer, that is repositionned everytime the :class:`MapView` is
-    moved.
-    """
-    viewport_x = NumericProperty(0)
-    viewport_y = NumericProperty(0)
-
-    def reposition(self):
-        """Function called when :class:`MapView` is moved. You must recalculate
-        the position of your children.
-        """
-        pass
-
-    def unload(self):
-        """Called when the view want to completly unload the layer.
-        """
-        pass
-
-
-class MarkerMapLayer(MapLayer):
-    """A map layer for :class:`MapMarker`
-    """
-    order_marker_by_latitude = BooleanProperty(True)
-
-    def __init__(self, **kwargs):
-        self.markers = []
-        super(MarkerMapLayer, self).__init__(**kwargs)
-
-    def insert_marker(self, marker, **kwargs):
-        if self.order_marker_by_latitude:
-            before = list(takewhile(
-                lambda i_m: i_m[1].lat < marker.lat,
-                enumerate(self.children)
-            ))
-            if before:
-                kwargs['index'] = before[-1][0] + 1
-
-        super(MarkerMapLayer, self).add_widget(marker, **kwargs)
-
-    def add_widget(self, marker):
-        marker._layer = self
-        self.markers.append(marker)
-        self.insert_marker(marker)
-
-    def remove_widget(self, marker):
-        marker._layer = None
-        if marker in self.markers:
-            self.markers.remove(marker)
-        super(MarkerMapLayer, self).remove_widget(marker)
-
-    def reposition(self):
-        if not self.markers:
-            return
-        mapview = self.parent
-        set_marker_position = self.set_marker_position
-        bbox = None
-        # reposition the markers depending the latitude
-        markers = sorted(self.markers, key=lambda x: -x.lat)
-        margin = max((max(marker.size) for marker in markers))
-        bbox = mapview.get_bbox(margin)
-        for marker in markers:
-            if bbox.collide(marker.lat, marker.lon):
-                set_marker_position(mapview, marker)
-                if not marker.parent:
-                    self.insert_marker(marker)
-            else:
-                super(MarkerMapLayer, self).remove_widget(marker)
-
-    def set_marker_position(self, mapview, marker):
-        x, y = mapview.get_window_xy_from(marker.lat, marker.lon, mapview.zoom)
-        marker.x = int(x - marker.width * marker.anchor_x)
-        marker.y = int(y - marker.height * marker.anchor_y)
-
-    def unload(self):
-        self.clear_widgets()
-        del self.markers[:]
-
-
 class DrawableMapView(Scatter):
     """
     Main MapView on which we will put our images
     and where we will draw
     """
+    lines = []
+    first_position = None
 
     def __init__(self, **kwargs):
         self.do_rotation = False
-        self.zoom = self.scale
-        self._default_marker_layer = None
-        self._layers = []
-        self.canvas = Canvas()
-        with self.canvas:
-            self.canvas_map = Canvas()
-            self.canvas_layers = Canvas()
-        with self.canvas:
-            self.canvas_layers_out = Canvas()
         super().__init__(**kwargs)
 
     def on_touch_down(self, touch):
+        """
+        When in draw_mode (TODO), make a line segment
+        path based on current touch and last touch
+        """
         x = touch.x
         y = touch.y
-        absolute_pos = self.to_local(x, y)
-        print(absolute_pos)
-        print(x, y)
+        (abs_x, abs_y) = self.to_local(x, y)
         with self.canvas:
-            Color(0, 0, 1, 1, mode='rgba')
-            self.rect = Rectangle(
-                pos=(absolute_pos[0], absolute_pos[1]), size=(50, 50))
+            Color(0, 0, 0, 1, mode='rgba')  # black
+            # Three cases:
+            # Case 1: no touches yet, so mark where first touch is
+            if not self.first_position:
+                self.first_position = (abs_x, abs_y)
+            else:
+                # Case 2: one touch so far, with this next touch make a line
+                # segment from first_position touch and current touch
+                if not self.lines:
+                    start_x = self.first_position[0]
+                    start_y = self.first_position[1]
+                    end_x = abs_x
+                    end_y = abs_y
+                    self.lines.append(
+                        Line(points=[start_x, start_y, end_x, end_y], width=5))
+                # Case 3: there are already line segments, so make a new line segment
+                # from our last made line segment's last endpoints and our current touch
+                else:
+                    last_line = self.lines[len(self.lines)-1]
+                    start_x = last_line.points[len(last_line.points)-2]
+                    start_y = last_line.points[len(last_line.points)-1]
+                    end_x = abs_x
+                    end_y = abs_y
+                    self.lines.append(
+                        Line(points=[start_x, start_y, end_x, end_y], width=5))
         super().on_touch_down(touch)
-
-    # def on_translation(self, touch):
-    #     x = touch.x
-    #     y = touch.y
-    #     print(x, y)
-    #     with self.canvas:
-    #         Color(1, 0, 0, 0.5, mode='rgba')
-    #         self.rect = Rectangle(pos=(touch.x, touch.y), size=(50, 50))
-    #     super().on_translation(touch)
 
     def collide_point(self, x, y):
         # print "collide_point", x, y
